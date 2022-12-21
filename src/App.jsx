c3801cebc1120f679b3717a9151b301b4c28a69a
Lúcio Cecílio de Souza Júnior <devluciocdesouza@gmail.com>
2022-12-21 20:28:30 -0300

    Criando login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Site from './site/Site';
+import Login from './app/login/Login';
 
 function App(){
     return <BrowserRouter>
     <Routes>
       <Route path='/' element={<Site/>}/>
+      <Route path='/app' element={<Login/>}/>
     </Routes>
     </BrowserRouter> ;
   }
